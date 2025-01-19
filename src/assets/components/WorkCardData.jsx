4a86c483d007e442a3ead1a00b401b3602419855
karthik k <karthikkarunakaran444@gmail.com>
2025-01-19 19:41:13 +0530

    .

--- src/assets/components/WorkCardData.jsx
+++ src/assets/components/WorkCardData.jsx
@@ -1,5 +1,6 @@
 import pro1 from "../Assets/Screenshot 2025-01-17 200120.png"
 import pro2 from "../Assets/ss2.png"
+import pro3 from "../Assets/ss3.png"
 
 const ProjectCardData= [
     {
@@ -15,6 +16,13 @@ const ProjectCardData= [
         text:"Welcome to my personal portfolio! I am a passionate and dedicated web developer with a keen interest in creating innovative and responsive web applications. I have honed my skills in various front-end technologies such as HTML, CSS, JavaScript, React, and Vite.",
         view:"https://karthikkarunakaran.netlify.app/",
         source:"https://github.com/karthik-k44/PORTFOLIO1_"
+    },
+    {
+        imgsrc:pro3,
+        title:"Calorie Calculator ",
+        text:"Calorie calculator is the one-stop solution for managing your daily caloric intake This project is designed to help you search for dishes and instantly access their calorie information, empowering you to make informed dietary choices. With an easy-to-use interface, our platform simplifies the process of tracking meals, ensuring a healthier lifestyle for users of all ages.",
+        view:"https://calorie-calculator-hackathon.netlify.app/",
+        source:"https://github.com/karthik-k44/Calorie_Calculator/"
     }
 ]
 
